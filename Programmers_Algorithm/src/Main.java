c36a0c7a4e52414fbc7714919c5f647d81faa938
psmoon7304 <psmoon7304@naver.com>
2025-03-17 05:16:45 +0900

    250317

--- Programmers_Algorithm/src/Main.java
+++ Programmers_Algorithm/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Solution5 run = new Solution5();
-        System.out.println(run.solution(4, 3, new int[] {4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2}));
+        Solution7 run = new Solution7();
+        System.out.println(run.solution(2,1,20));
     }
 }
---
+++ Programmers_Algorithm/src/Solution7.java
@@ -0,0 +1,12 @@
+public class Solution7 {
+    public int solution(int a, int b, int n) {
+        int answer = 0, temp = 0;
+        while(n / a != 0) {
+            answer += n / a * b;
+            temp = n % a;
+            n = n / a * b;
+            n += temp;
+        }
+        return answer;
+    }
+}

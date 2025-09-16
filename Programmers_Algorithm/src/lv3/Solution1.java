3eeffec2ae36b517b02488f80eb3ed4e396228f5
psmoon7304 <psmoon7304@naver.com>
2025-09-16 14:57:00 +0900

    250916

--- Programmers_Algorithm/src/lv3/Solution1.java
+++ Programmers_Algorithm/src/lv3/Solution1.java
@@ -7,20 +7,20 @@ public class Solution1 {
     static int[] tree;
     static boolean[] isVisited;
 
-    static void searchLeaf(int pointer) {
+    static void dfs(int pointer) {
         ArrayList<Integer> treeInfo = road[pointer];
         isVisited[pointer] = true;
 
         for(Integer i : treeInfo) {
             if(isVisited[i]) continue;
-            searchLeaf(i);
+            dfs(i);
         }
-        if(tree[pointer] == 1) return;
-        if(treeInfo.size() == 1) {
+        if(tree[pointer] == 1) return; // 현재 위치가 이미 등대라면 더 이상 볼 필요없이 종료
+        if(treeInfo.size() == 1) { // 단말 노드라면 부모 노드가 등대로
             tree[treeInfo.get(0)] = 1;
             return;
         }
-        for(Integer i : treeInfo) {
+        for(Integer i : treeInfo) { // 현재 노드와 간선으로 연결된 노드들 중 등대가 아닌 노드가 있다면 해당 노드를 등대로
             if(tree[i] == 1) continue;
             tree[i] = 1;
         }
@@ -40,14 +40,14 @@ public class Solution1 {
         }
     }
 
-    static int getAnswer(int n) {
+    static int getAnswer(int n) { // tree 배열을 반복문을 통하여 1, 즉 등대를 찾아 개수를 구함
         int answer = 0;
         for(int i = 1; i <= n; i++) answer += tree[i] == 1? 1 : 0;
         return answer;
     }
     public int solution(int n, int[][] input) {
         init(n, input);
-        searchLeaf(1);
+        dfs(1);
         return getAnswer(n);
     }
 }

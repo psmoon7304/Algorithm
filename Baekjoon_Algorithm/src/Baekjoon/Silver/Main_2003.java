3f9bf4d3b525e5bd202d6962222614b33d8fe888
psmoon7304 <psmoon7304@naver.com>
2025-10-17 12:29:43 +0900

    [feat]백준 2003.수들의 합 2

---
+++ Baekjoon_Algorithm/src/Baekjoon/Silver/Main_2003.java
@@ -0,0 +1,41 @@
+package Baekjoon.Silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2003 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int N, M, answer;
+    static int[] prefix;
+
+    private static int getResult() {
+        int start =1;
+        for(int end = 1; end <= N; end++) {
+            while(M <= (prefix[end] - prefix[start - 1])) {
+                if(M == (prefix[end] - prefix[start - 1])) answer++;
+                start++;
+            }
+        }
+        return answer;
+    }
+
+    private static void prefixSum() throws Exception {
+        st = new StringTokenizer(br.readLine().trim());
+        for(int i = 1; i <= N; i++) prefix[i] = prefix[i - 1] + Integer.parseInt(st.nextToken());
+    }
+
+    private static void init() throws Exception {
+        st = new StringTokenizer(br.readLine().trim());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        prefix = new int[N + 1];
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+        prefixSum();
+        System.out.println(getResult());
+    }
+}

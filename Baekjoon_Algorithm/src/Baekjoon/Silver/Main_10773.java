f124e9a3553d51040431557c5594c407a14f0e28
psmoon7304 <psmoon7304@naver.com>
2025-10-19 21:01:56 +0900

    [feat]백준 10773.제로

---
+++ Baekjoon_Algorithm/src/Baekjoon/Silver/Main_10773.java
@@ -0,0 +1,25 @@
+package Baekjoon.Silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_10773 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int[] prefixSum;
+    static int K, pointer = 1;
+
+    public static void main(String[] args) throws Exception {
+        K = Integer.parseInt(br.readLine().trim());
+        prefixSum = new int[K + 1];
+        for(int i = 0; i < K; i++) {
+            int value = Integer.parseInt(br.readLine().trim());
+            if(value == 0) prefixSum[--pointer] = 0;
+            else {
+                prefixSum[pointer] = prefixSum[pointer - 1] + value;
+                pointer += 1;
+            }
+        }
+        System.out.println(prefixSum[pointer - 1]);
+    }
+}

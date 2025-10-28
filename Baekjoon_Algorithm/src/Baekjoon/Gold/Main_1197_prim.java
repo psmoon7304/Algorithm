1c3c25fee5ac86e3f6d46643d4a24a82a87f2807
psmoon7304 <psmoon7304@naver.com>
2025-10-28 17:28:41 +0900

    [feat]백준 18808.스티커 붙이기

--- Baekjoon_Algorithm/src/Baekjoon/Gold/Main_1197_prim.java
+++ Baekjoon_Algorithm/src/Baekjoon/Gold/Main_1197_prim.java
@@ -1,4 +1,26 @@
 package Baekjoon.Gold;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.PriorityQueue;
+
 public class Main_1197_prim {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static PriorityQueue<Edge> pq;
+
+    static int V, E, edgeCount;
+    static long answer;
+
+    static class Edge implements Comparable<Edge>{
+        int end;
+        int cost;
+        Edge(int end, int cost) {
+            this.end = end;
+            this.cost = cost;
+        }
+        @Override
+        public int compareTo(Edge o) {return Integer.compare(this.cost, o.cost);}
+    }
 }

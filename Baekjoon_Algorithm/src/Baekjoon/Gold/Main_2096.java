42f6b1f5f5c51a6696c80d32342e1f1cbf2d8821
psmoon7304 <psmoon7304@naver.com>
2025-10-30 17:49:02 +0900

    [feat]백준 2096.내려가기

---
+++ Baekjoon_Algorithm/src/Baekjoon/Gold/Main_2096.java
@@ -0,0 +1,54 @@
+package Baekjoon.Gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_2096 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+    static int[] map;
+    static int[] minArray;
+    static int[] maxArray;
+
+    private static void getResult() throws Exception{
+        int[] tempMin = new int[] {0, 0, 0}, tempMax = new int[] {0, 0, 0};
+        map = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        for(int i = 0; i < 3; i++) {
+            minArray[i] = map[i];
+            maxArray[i] = map[i];
+        }
+
+        for(int i = 1; i < N; i++) {
+            map = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            tempMin[0] = Math.min(map[0] + minArray[0], map[0] + minArray[1]);
+            tempMin[1] = Math.min(Math.min(map[1] + minArray[0], map[1] + minArray[1]),map[1] + minArray[2]);
+            tempMin[2] = Math.min(map[2] + minArray[1], map[2] + minArray[2]);
+
+            tempMax[0] = Math.max(map[0] + maxArray[0], map[0] + maxArray[1]);
+            tempMax[1] = Math.max(Math.max(map[1] + maxArray[0], map[1] + maxArray[1]),map[1] + maxArray[2]);
+            tempMax[2] = Math.max(map[2] + maxArray[1], map[2] + maxArray[2]);
+
+            for(int j = 0; j < 3; j++) {
+                minArray[j] = tempMin[j];
+                maxArray[j] = tempMax[j];
+            }
+        }
+    }
+
+    private static void init() throws Exception {
+        N = Integer.parseInt(br.readLine().trim());
+        map = new int[3];
+        minArray = new int[3];
+        maxArray = new int[3];
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+        getResult();
+        sb.append(Math.max(Math.max(maxArray[0], maxArray[1]),maxArray[2])).append(" ").append(Math.min(Math.min(minArray[0], minArray[1]),minArray[2]));
+        System.out.println(sb.toString());
+    }
+}

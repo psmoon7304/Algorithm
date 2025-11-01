fd3a392a80990840a8653e211c236e8dfdc634ca
psmoon7304 <psmoon7304@naver.com>
2025-11-01 21:41:29 +0900

    [feat]백준 2096.내려가기 시간 단축

--- Baekjoon_Algorithm/src/Baekjoon/Gold/Main_2096.java
+++ Baekjoon_Algorithm/src/Baekjoon/Gold/Main_2096.java
@@ -2,11 +2,12 @@ package Baekjoon.Gold;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main_2096 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
 
     static int N;
     static int[] map;
@@ -15,21 +16,22 @@ public class Main_2096 {
 
     private static void getResult() throws Exception{
         int[] tempMin = new int[] {0, 0, 0}, tempMax = new int[] {0, 0, 0};
-        map = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        st = new StringTokenizer(br.readLine().trim());
         for(int i = 0; i < 3; i++) {
-            minArray[i] = map[i];
-            maxArray[i] = map[i];
+            map[i] = Integer.parseInt(st.nextToken());
+            minArray[i] = maxArray[i] = map[i];
         }
 
         for(int i = 1; i < N; i++) {
-            map = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            tempMin[0] = Math.min(map[0] + minArray[0], map[0] + minArray[1]);
-            tempMin[1] = Math.min(Math.min(map[1] + minArray[0], map[1] + minArray[1]),map[1] + minArray[2]);
-            tempMin[2] = Math.min(map[2] + minArray[1], map[2] + minArray[2]);
+            st = new StringTokenizer(br.readLine().trim());
+            for(int j = 0; j < 3; j++) map[j] = Integer.parseInt(st.nextToken());
+            tempMin[0] = map[0] + Math.min(minArray[0], minArray[1]);
+            tempMin[1] = map[1] + Math.min(Math.min(minArray[0], minArray[1]),minArray[2]);
+            tempMin[2] = map[2] + Math.min(minArray[1], minArray[2]);
 
-            tempMax[0] = Math.max(map[0] + maxArray[0], map[0] + maxArray[1]);
-            tempMax[1] = Math.max(Math.max(map[1] + maxArray[0], map[1] + maxArray[1]),map[1] + maxArray[2]);
-            tempMax[2] = Math.max(map[2] + maxArray[1], map[2] + maxArray[2]);
+            tempMax[0] = map[0] + Math.max(maxArray[0],maxArray[1]);
+            tempMax[1] = map[1] + Math.max(Math.max(maxArray[0], maxArray[1]), maxArray[2]);
+            tempMax[2] = map[2]+ Math.max(maxArray[1], maxArray[2]);
 
             for(int j = 0; j < 3; j++) {
                 minArray[j] = tempMin[j];

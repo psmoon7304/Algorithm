89d7034428a1c1bbf1813b2a6fa0d9cd47410175
psmoon7304 <psmoon7304@naver.com>
2025-12-08 01:46:58 +0900

    [feat]백준 13549.숨바꼭질 3

---
+++ Baekjoon_Algorithm/src/Baekjoon/Gold/Main_13549.java
@@ -0,0 +1,43 @@
+package Baekjoon.Gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Main_13549 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int N, K;
+    static final int MAX = 100000;
+    static boolean[] isVisited;
+
+    private static int bfs(int X, int time) {
+        Deque<int[]> que = new LinkedList<>();
+        que.offer(new int[] {X, time});
+
+        while(!que.isEmpty()) {
+            int[] temp = que.poll();
+            isVisited[temp[0]] = true;
+            if(temp[0] == K) return temp[1];
+            if(temp[0] * 2 <= MAX && !isVisited[temp[0] * 2 ]) que.addFirst(new int[] {2 * temp[0], temp[1]});
+            if(temp[0] - 1 >= 0 && !isVisited[temp[0] - 1]) que.offer(new int[] {temp[0] - 1, temp[1] + 1});
+            if(temp[0] + 1 <= MAX && !isVisited[temp[0] + 1]) que.offer(new int[] {temp[0] + 1, temp[1] + 1});
+        }
+        return 0;
+    }
+
+    private static void init() throws Exception {
+        st = new StringTokenizer(br.readLine().trim());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        isVisited = new boolean[MAX + 1];
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+        System.out.print(bfs(N, 0));
+    }
+}

3e50cfc920876ada216a2a0fb3fb721bce89bec9
psmoon7304 <psmoon7304@naver.com>
2026-01-04 04:30:03 +0900

    [feat]백준 28292.개미 수열

---
+++ Baekjoon_Algorithm/src/Baekjoon/Silver/Main_28292ver2.java
@@ -0,0 +1,14 @@
+package Baekjoon.Silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_28292ver2 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        int num = Integer.parseInt(br.readLine().trim());
+        if(num <= 2) System.out.print(1);
+        else if(num <= 5) System.out.print(2);
+        else System.out.print(3);
+    }
+}

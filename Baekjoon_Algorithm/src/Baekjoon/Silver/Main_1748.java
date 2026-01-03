1f2981907744011c899072edc0fe1bdcd313d968
psmoon7304 <psmoon7304@naver.com>
2026-01-03 23:10:01 +0900

    [feat]백준 1748.수 이어 쓰기 1

---
+++ Baekjoon_Algorithm/src/Baekjoon/Silver/Main_1748.java
@@ -0,0 +1,21 @@
+package Baekjoon.Silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_1748 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        String stringNum = br.readLine().trim();
+        int numLength = stringNum.length() - 1;
+        int num = Integer.parseInt(stringNum);
+        int answer = 0;
+        for(int i = 0; i < numLength; i++) {
+            int temp = (int)(Math.pow(10, i) * 9);
+            num -= temp;
+            answer += temp * (i + 1);
+        }
+        System.out.print(answer + (num * (numLength + 1)));
+    }
+}

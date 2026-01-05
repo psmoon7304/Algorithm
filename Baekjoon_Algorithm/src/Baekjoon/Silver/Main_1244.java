fb4845706036b97c117ee5ea3ed1ed96cf7bff40
psmoon7304 <psmoon7304@naver.com>
2026-01-05 23:42:47 +0900

    [feat]백준 1244.스위치 켜고 끄기

---
+++ Baekjoon_Algorithm/src/Baekjoon/Silver/Main_1244.java
@@ -0,0 +1,53 @@
+package Baekjoon.Silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1244 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int switchNum;
+    static int[] switchStatus;
+
+    private static void man(int num) {
+        for(int i = num; i <= switchNum; i += num) switchStatus[i] *= -1;
+    }
+
+    private static void woman(int num) {
+        int start = num, end = num;
+        while(start > 1 && end < switchNum) {
+            if(switchStatus[start - 1] != switchStatus[end + 1]) break;
+            start--;
+            end++;
+        }
+        if(start <= 0) start = 1;
+        if(end > switchNum) end = switchNum;
+        for(int i = start; i <= end; i++) switchStatus[i] *= -1;
+    }
+
+    private static void init() throws Exception {
+        switchNum = Integer.parseInt(br.readLine().trim());
+        switchStatus = new int[switchNum + 1];
+
+        st = new StringTokenizer(br.readLine().trim());
+        for(int i = 1; i <= switchNum; i++) switchStatus[i] = Integer.parseInt(st.nextToken()) == 0 ? -1 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+        int player = Integer.parseInt(br.readLine().trim());
+        for(int i = 0; i < player; i++) {
+            st = new StringTokenizer(br.readLine().trim());
+            if(Integer.parseInt(st.nextToken()) == 1) man(Integer.parseInt(st.nextToken()));
+            else woman(Integer.parseInt(st.nextToken()));
+        }
+        for(int i = 1; i <= switchNum; i++) {
+            sb.append(switchStatus[i] == -1 ? 0 : 1).append(" ");
+            if(i % 20 == 0) sb.append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+}

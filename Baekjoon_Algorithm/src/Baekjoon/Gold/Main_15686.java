137362d742c2563959947136df1683ab26723893
psmoon7304 <psmoon7304@naver.com>
2026-01-13 21:16:22 +0900

    [feat]백준 15686.치킨 배달

--- Baekjoon_Algorithm/src/Baekjoon/Gold/Main_15686.java
+++ Baekjoon_Algorithm/src/Baekjoon/Gold/Main_15686.java
@@ -3,40 +3,37 @@ package Baekjoon.Gold;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.LinkedList;
-import java.util.Deque;
 
 public class Main_15686 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
 
-    static Deque<int[]> que;
-    static int N, M, answer = Integer.MAX_VALUE;
+    static int N, M, answer = Integer.MAX_VALUE, storeSize, houseSize;
     static int[][] housePos;
-    static int[][] distance;
+    static int[][] storePos;
+    static int[] pickStore;
 
-    private static void getStoreDis() {
-        int index = 0;
-        while(!que.isEmpty()) {
-            int[] temp = que.poll();
-            index++;
-            for(int i = 0; i < housePos[0].length; i++) {
-                int dis = Math.abs(housePos[i][0] - temp[0]) + Math.abs(housePos[i][1] - temp[1]);
-                distance[i][index] = dis;
+    private static int getStoreDis() {
+        int sum = 0;
+        for(int i = 0; i < houseSize; i++) {
+            int temp = Integer.MAX_VALUE;
+            for(int j = 0; j < M; j++) {
+                temp = Math.min(Math.abs(housePos[i][0] - storePos[pickStore[j]][0]) + Math.abs(housePos[i][1] - storePos[pickStore[j]][1]), temp);
             }
+            sum += temp;
         }
+        return sum;
     }
 
-    private static void combi(int d, int s, int result) {
+    private static void combi(int d, int s) {
         if(d == M) {
-            answer = Math.min(answer, result);
+            answer = Math.min(answer, getStoreDis());
             return;
         }
 
-        for(int i = s; i < distance[0].length; i++) {
-            for(int j = 0; j < distance.length; j++) {
-                combi(d + 1, s + 1, result + distance[j][i]);
-            }
+        for(int i = s; i < storeSize; i++) {
+            pickStore[d] = i;
+            combi(d + 1, i + 1);
         }
     }
 
@@ -46,25 +43,35 @@ public class Main_15686 {
         M = Integer.parseInt(st.nextToken());
 
         housePos = new int[2 * N][2];
-        que = new LinkedList<>();
+        storePos = new int[13][2];
+        pickStore = new int[M];
 
-        int houseIndex = 0, temp;
+        int temp;
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine().trim());
             for(int j = 0; j < N; j++) {
                 temp = Integer.parseInt(st.nextToken());
-                if(temp == 1) housePos[houseIndex++] = new int[] {i, j};
-                else if(temp == 2) que.offer(new int[] {i, j});
+                if(temp == 1) housePos[houseSize++] = new int[] {i, j};
+                else if(temp == 2) {
+                    storePos[storeSize++] = new int[] {i, j};
+                }
             }
         }
-        distance = new int[--houseIndex][que.size()];
     }
 
     public static void main(String[] args) throws Exception {
         init();
-        getStoreDis();
-        combi(0, 0, 0);
+        combi(0, 0);
         System.out.println(answer);
     }
 
 }
+
+/*
+    M개를 제외한 나머지는 폐업
+    M개를 먼저 선발 후 해당 치킨집을 가지고 거리 계산 후 해당 거리가 가장 짧은 애가 정답
+    집 위치를 저장 housePos
+    가게 위치를 저장 storePos
+
+    combie 조합으로 먼저 경우의 수를 뽑고 해당 경우의 수로 정답을 구함
+ */

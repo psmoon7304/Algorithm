4f88742a26cbbb7c3325bc330ab209953f1c2e49
psmoon7304 <psmoon7304@naver.com>
2026-01-17 22:21:10 +0900

    [feat]SWEA 25310.전송시간-추가

---
+++ SWExpertAcademy_Algorithm/src/b/전송시간_25310/UserSolution.java
@@ -0,0 +1,21 @@
+package b.전송시간_25310;
+
+class UserSolution {
+
+    public void init(int N, int K, int mNodeA[], int mNodeB[], int mTime[])
+    {
+    }
+
+    public void addLine(int mNodeA, int mNodeB, int mTime)
+    {
+    }
+
+    public void removeLine(int mNodeA, int mNodeB)
+    {
+    }
+
+    public int checkTime(int mNodeA, int mNodeB)
+    {
+        return 0;
+    }
+}

31e1b738a7f6f187f4a4d544cc67f5bdbc8dee15
psmoon7304 <psmoon7304@naver.com>
2026-02-13 14:28:11 +0900

    [feat]백준 1486.등산

--- Baekjoon_Algorithm/src/Baekjoon/Gold/Main_1486/Main_1486.java
+++ Baekjoon_Algorithm/src/Baekjoon/Gold/Main_1486/Main_1486.java
@@ -3,24 +3,75 @@ package Baekjoon.Gold.Main_1486;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
+class Node implements Comparable<Node> {
+    int e, w;
+    public Node(int e, int w) {
+        this.e = e;
+        this.w = w;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return this.w - o.w;
+    }
+}
 
 public class Main_1486 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
+    static PriorityQueue<Node> pq;
 
-    static int N, M, T, D, answer, moveTime;
-    static int[][] map;
+    static int[] map;
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
+    static int N, M, T, D;
 
-    private static int changeASCII(char c) {
-        if(c <= 'Z') return c - 65;
-        return c - 71;
+    private static int[] dijk(int start, boolean isReverse) {
+        int[] dijkMap = new int[N * M + 1];
+        Arrays.fill(dijkMap, Integer.MAX_VALUE);
+        dijkMap[start] = 0;
+        pq.clear();
+        pq.offer(new Node(start, 0));
+
+        while(!pq.isEmpty()) {
+            Node node = pq.poll();
+            if(dijkMap[node.e] < node.w) continue;
+
+            int tempR = node.e / M;
+            int tempC = node.e % M;
+            for(int i = 0; i < 4; i++) {
+                int r = tempR + dr[i];
+                int c = tempC + dc[i];
+                int nextCost = 1;
+                if(!check(r, c) || Math.abs(map[node.e] - map[r * M + c]) > T) continue;
+                if(isReverse) {
+                    if(map[node.e] <= map[r * M + c]) nextCost = 1;
+                    else nextCost = (int)Math.pow(map[node.e] - map[r * M + c], 2);
+                } else {
+                    if(map[node.e] >= map[r * M + c]) nextCost = 1;
+                    else nextCost = (int)Math.pow(map[r * M + c] - map[node.e], 2);
+                }
+
+                if(dijkMap[r * M + c] <= dijkMap[node.e] + nextCost) continue;
+                dijkMap[r * M + c] = dijkMap[node.e] + nextCost;
+                pq.offer(new Node(r * M + c, dijkMap[node.e] + nextCost));
+            }
+        }
+        return dijkMap;
     }
 
-    private static void changeIntMap(char[][] tempMap) {
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) map[i][j] = changeASCII(tempMap[i][j]);
+    private static boolean check(int r, int c) {
+        return r >= 0 && r < N && c >= 0 && c < M;
+    }
+
+    private static int changeASCII(char c) {
+        if(c <= 'Z') {
+            return c - 65;
         }
+        return c - 71;
     }
 
     private static void init() throws Exception {
@@ -30,23 +81,29 @@ public class Main_1486 {
         T = Integer.parseInt(st.nextToken());
         D = Integer.parseInt(st.nextToken());
 
-        map = new int[N][M];
-        char[][] tempMap = new char[N][M];
-        for(int i = 0; i < N; i++) tempMap[i] = br.readLine().toCharArray();
-        changeIntMap(tempMap);
-    }
+        pq = new PriorityQueue<>();
+        map = new int[N * M];
 
-    private static void test() {
-        for(int i = 0; i < N; i++) {
+        for(int i = 0; i < N ; i++) {
+            char[] input = br.readLine().toCharArray();
             for(int j = 0; j < M; j++) {
-                System.out.print(map[i][j] + " ");
+                map[i * M + j] = changeASCII(input[j]);
             }
-            System.out.println();
         }
     }
 
     public static void main(String[] args) throws Exception {
         init();
-        test();
+        int[] startDijk = dijk(0, false);
+        int[] reverseDijk = dijk(0, true);
+        int answer = 0;
+        for(int i = 0; i < N * M; i++) {
+            if(startDijk[i] != Integer.MAX_VALUE && reverseDijk[i] != Integer.MAX_VALUE) {
+                long temp = startDijk[i] + reverseDijk[i];
+                if(temp > D) continue;
+                answer = Math.max(answer, map[i]);
+            }
+        }
+        System.out.print(answer);
     }
 }
